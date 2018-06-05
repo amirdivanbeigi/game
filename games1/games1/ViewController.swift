ecb7c3dc025f7592e1310a13ef867f262f39c658
amirdivanbeigi <32493593+amirdivanbeigi@users.noreply.github.com>
2018-06-05 12:34:04 +0430

    change from t3

--- games1/games1/ViewController.swift
+++ games1/games1/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("hello world")
         print("sdsdsd")
+        print("bye world")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
