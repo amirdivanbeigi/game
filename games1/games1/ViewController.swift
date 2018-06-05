3b155cb808c04921e40c5cd1ea6af233129d4684
amirdivanbeigi <32493593+amirdivanbeigi@users.noreply.github.com>
2018-06-05 11:34:23 +0430

    first

---
+++ games1/games1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  games1
+//
+//  Created by amir on 6/5/18.
+//  Copyright © 2018 phototager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
